e3c3f6f2e5c41ecc2d8d4d6dfd823430b45f859f
4e5pik1 <124652735+4e5pik1@users.noreply.github.com>
2023-07-21 14:19:03 +0530

    Add files via upload

---
+++ Z.java.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Z
+{
+public static void main(String args[])
+{
+int i,j;
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter the no of row ");
+int n=s.nextInt();
+
+for(i=1;i<=n;i++)
+{
+if(i==1||i==n)
+{
+for(j=1;j<=n;j++)
+{
+System.out.print("@ ");
+}
+
+System.out.println(" ");
+
+}
+
+else
+{
+for(j=1;j<=n-i;j++)
+{
+System.out.print("  ");
+
+}
+System.out.print("#");
+System.out.println(" ");
+}
+}
+}
+}
